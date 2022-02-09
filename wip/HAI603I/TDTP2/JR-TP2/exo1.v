51a7c894582123178964ad7818ba4ddb7ee87151
Johann Rosain <johann.rosain@gmail.com>
2022-02-09 22:15:59 +0100

    [JR] [HAI603I] [#11] TDTP2 preuves dans LJ(eq)

---
+++ wip/HAI603I/TDTP2/JR-TP2/exo1.v
@@ -0,0 +1,82 @@
+Open Scope type_scope.
+
+Section iso_axioms.
+
+Variables A B C : Set.
+
+Axiom Com : A * B = B * A.
+Axiom Ass : A * (B * C) = A * B * C.
+Axiom Cur : (A * B -> C) = (A -> B -> C).
+Axiom Dis : (A -> B * C) = (A -> B) * (A -> C).
+Axiom P_unit : A * unit = A.
+Axiom AR_unit : (A -> unit) = unit.
+Axiom AL_unit : (unit -> A) = A.
+
+End iso_axioms.
+
+Lemma isos_ex1 : forall A B : Set, A * (B -> unit) = A.
+Proof.
+  intros.
+  rewrite AR_unit.
+  rewrite P_unit.
+  reflexivity.
+Qed.
+
+Lemma isos_ex2 : forall A B : Set, A * unit * B = B * (unit * A).
+Proof.
+  intros.
+  rewrite P_unit.
+  rewrite Ass.
+  rewrite P_unit.
+  rewrite Com.
+  reflexivity.
+Qed.
+
+Lemma isos_ex3 : forall A B C : Set, (A * unit -> B * (C * unit)) = (A * unit -> (C -> unit) * C) * (unit -> A -> B).
+Proof.
+  intros.
+  rewrite Ass.
+  rewrite P_unit.
+  rewrite P_unit.
+  rewrite Dis.
+  rewrite AL_unit.
+  rewrite AR_unit.
+  rewrite Dis.
+  rewrite AR_unit.
+  rewrite (Com unit).
+  rewrite P_unit.
+  rewrite Com.
+  reflexivity.
+Qed.
+
+Lemma P_unit_inv : forall A : Set, (unit * A) = A.
+Proof.
+  intros.
+  rewrite Com.
+  rewrite P_unit.
+  reflexivity.
+Qed.
+
+Ltac simplify := 
+  intros ; 
+  repeat (rewrite P_unit || rewrite AR_unit || rewrite AL_unit || rewrite P_unit_inv || rewrite Dis) ;
+  try reflexivity.
+
+Lemma isos_ex1_tac : forall A B : Set, A * (B -> unit) = A.
+Proof.
+  simplify.
+Qed.
+
+Lemma isos_ex2_tac : forall A B : Set, A * unit * B = B * (unit * A).
+Proof.
+  simplify.
+  rewrite Com.
+  reflexivity.
+Qed.
+
+Lemma isos_ex3_tac : forall A B C : Set, (A * unit -> B * (C * unit)) = (A * unit -> (C -> unit) * C) * (unit -> A -> B).
+Proof.
+  simplify.
+  rewrite Com.
+  reflexivity.
+Qed.
---
+++ wip/HAI603I/TDTP2/JR-TP2/exo2.v
@@ -0,0 +1,82 @@
+Section Peano.
+
+Parameter N : Set.
+Parameter o : N.
+Parameter s : N -> N.
+Parameters plus mult : N -> N -> N.
+Variables x y : N.
+Axiom ax1 : ~ ((s x) = o).
+Axiom ax2 : exists z, ~(x = o) -> (s z) = x.
+Axiom ax3 : (s x) = (s y) -> x = y.
+Axiom ax4 : (plus x o) = x.
+Axiom ax5 : (plus x (s y)) = s (plus x y).
+Axiom ax6 : (mult x o) = o.
+Axiom ax7 : (mult x (s y)) = (plus (mult x y) x).
+
+End Peano.
+
+Lemma peano_ex1 : (plus (s o) (s (s o))) = (s (s (s o))).
+Proof.
+  rewrite ax5.
+  rewrite ax5.
+  rewrite ax4.
+  reflexivity.
+Qed.
+
+Lemma peano_ex2 : (plus (s (s o)) (s (s o))) = (s (s (s (s o)))).
+Proof.
+  rewrite ax5.
+  rewrite ax5.
+  rewrite ax4.
+  reflexivity.
+Qed.
+
+Lemma peano_ex3 : (mult (s (s o)) (s (s o))) = (s (s (s (s o)))).
+Proof.
+  rewrite ax7.
+  rewrite ax5.
+  rewrite ax5.
+  rewrite ax4.
+  rewrite ax7.
+  rewrite ax6.
+  rewrite ax5.
+  rewrite ax5.
+  rewrite ax4.
+  reflexivity.
+Qed.
+
+Ltac simplify :=
+  repeat (rewrite ax4 || rewrite ax5 || rewrite ax6 || rewrite ax7) ;
+  try reflexivity.
+
+Lemma peano_ex1_tac : (plus (s o) (s (s o))) = (s (s (s o))).
+Proof.
+  simplify.
+Qed.
+
+Lemma peano_ex2_tac : (plus (s (s o)) (s (s o))) = (s (s (s (s o)))).
+Proof.
+  simplify.
+Qed.
+
+Lemma peano_ex3_tac : (mult (s (s o)) (s (s o))) = (s (s (s (s o)))).
+Proof.
+  simplify.
+Qed.
+
+Hint Rewrite ax4 ax5 ax6 ax7 : PeanoEq. 
+
+Lemma peano_ex1_autor : (plus (s o) (s (s o))) = (s (s (s o))).
+Proof.
+  autorewrite with PeanoEq using try reflexivity.
+Qed.
+
+Lemma peano_ex2_autor : (plus (s (s o)) (s (s o))) = (s (s (s (s o)))).
+Proof.
+  autorewrite with PeanoEq using try reflexivity.
+Qed.
+
+Lemma peano_ex3_autor : (mult (s (s o)) (s (s o))) = (s (s (s (s o)))).
+Proof.
+  autorewrite with PeanoEq using try reflexivity.
+Qed.
