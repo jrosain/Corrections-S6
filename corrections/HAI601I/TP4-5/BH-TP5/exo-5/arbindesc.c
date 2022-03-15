d7c928099eb4fb3c070e2896a7dcbf613d27d537
Huftier Benoît <ephesiel@gmail.com>
2022-03-15 12:49:09 +0100

    [BH] [HAI601I] Ajout TP5

---
+++ corrections/HAI601I/TP4-5/BH-TP5/exo-5/arbindesc.c
@@ -0,0 +1,76 @@
+/**
+ * @file analdesc.c        
+ * @author Michel Meynard
+ * @brief Analyse descendante récursive d'expression arithmétique
+ *
+ * Ce fichier contient un reconnaisseur d'expressions arithmétiques composée de 
+ * littéraux entiers sur un car, des opérateurs +, * et du parenthésage ().
+ * Remarque : soit rediriger en entrée un fichier, soit terminer par deux 
+ * caractères EOF (Ctrl-D), un pour lancer la lecture, l'autre comme "vrai" EOF.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include "arbin.h"
+
+/* les macros sont des blocs : pas de ';' apres */
+#define AVANCER {jeton=getchar();numcar++;}
+#define TEST_AVANCE(prevu) {if (jeton==(prevu)) AVANCER else ERREUR_SYNTAXE}
+#define ERREUR_SYNTAXE {printf("\nMot non reconnu : erreur de syntaxe \
+au caractère numéro %d \n",numcar); exit(1);} 
+
+Arbin E(void); Arbin R(Arbin); Arbin T(void); Arbin S(Arbin); Arbin F(void); /* déclars */
+
+int jeton;                      /* caractère courant du flot d'entrée */
+int numcar = 0;                 /* numero du caractère courant (jeton) */
+
+Arbin E(void) {
+  return R(T());                /* regle : E->TR */
+}
+
+Arbin R(Arbin g) {
+    if (jeton == '+') {         /* regle : R->+TR */
+        AVANCER
+        return R(ab_construire('+', g, T()));
+    }
+    else return g;              /* regle : R->epsilon */
+}
+
+Arbin T(void) {
+    return S(F());              /* regle : T->FS */
+}
+
+Arbin S(Arbin g) {
+    if (jeton == '*') {         /* regle : S->*FS */
+        AVANCER
+        return S(ab_construire('*', g, F()));
+    }
+    else return g;               /* regle : S->epsilon */
+}
+
+Arbin F(void) {
+    if (jeton == '(') {         /* regle : F->(E) */
+        AVANCER
+        Arbin a = E();
+        TEST_AVANCE(')')
+        return a;
+    }
+    else if (isdigit(jeton)) {  /* regle : F->0|1|...|9 */
+        Arbin a = ab_construire(jeton, NULL, NULL);
+        AVANCER
+        return a;
+    }
+    else ERREUR_SYNTAXE
+}
+
+int main(void) {                /* Fonction principale */
+    AVANCER			            /* initialiser jeton sur le premier car */
+    Arbin a = E();              /* axiome */
+    if (jeton == EOF) {         /* expression reconnue et rien après */
+        printf("\nMot reconnu\n");
+        ab_afficher(a);
+    }
+    else ERREUR_SYNTAXE         /* expression reconnue mais il reste des car */
+    return 0;
+}
